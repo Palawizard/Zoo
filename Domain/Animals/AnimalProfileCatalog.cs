552bc747917bd6e8b8bc5706425b64f92ac05a90
pmathys <mathys.paillekrobb@ynov.com>
2026-02-16 19:46:51 +0100

    feat(AnimalProfileCatalog) : adding other animals

--- Domain/Animals/AnimalProfileCatalog.cs
+++ Domain/Animals/AnimalProfileCatalog.cs
@@ -2,20 +2,101 @@ public static class AnimalProfileCatalog
 {
     private static readonly Dictionary<(SpeciesType species, SexType sex), AnimalProfile> Profiles = new()
     {
+        // Tiger
         {
-            (SpeciesType.Tigre, SexType.Male),
+            (SpeciesType.Tiger, SexType.Male),
             new AnimalProfile(
-                SpeciesType.Tigre, SexType.Male, FoodType.Meat,
+                SpeciesType.Tiger, SexType.Male, FoodType.Meat,
                 12m, 2, 6 * 365, null, 14 * 365, null, 25 * 365,
                 false, null, null, null, null)
         },
         {
-            (SpeciesType.Tigre, SexType.Female),
+            (SpeciesType.Tiger, SexType.Female),
             new AnimalProfile(
-                SpeciesType.Tigre, SexType.Female, FoodType.Meat, 
+                SpeciesType.Tiger, SexType.Female, FoodType.Meat, 
                 10m, 2, 4 * 365, 3, 14 * 365, 0.33m, 25 * 365,
                 false, 3, 20, null, null) 
         },
+        {
+        (SpeciesType.Eagle, SexType.Male),
+        new AnimalProfile(
+            SpeciesType.Eagle, 
+            SexType.Male, 
+            FoodType.Meat, 
+            0.25m,           
+            10,                    
+            4 * 365,                
+            null,            
+            14 * 365,       
+            null,                   
+            25 * 365,              
+            true,               
+            null,                  
+            null,                 
+            null,               
+            null                  
+        )
+        },
+            {
+        (SpeciesType.Eagle, SexType.Female),
+        new AnimalProfile(
+            SpeciesType.Eagle, 
+            SexType.Female, 
+            FoodType.Meat, 
+            0.3m,                  
+            10,                    
+            4 * 365,           
+            45,                   
+            14 * 365,               
+            0.5m,                  
+            25 * 365,             
+            true,                  
+            2,                     
+            null,                  
+            null,                  
+            3                      
+        )
+    },
+    {
+    (SpeciesType.Rooster, SexType.Female),
+    new AnimalProfile(
+        SpeciesType.Rooster, 
+        SexType.Female, 
+        FoodType.Seeds, 
+        0.15m,                 
+        1,                     
+        6 * 30,                
+        42,                  
+        8 * 365,               
+        0.5m,                 
+        15 * 365,               
+        false,                
+        null,                
+        null,                 
+        200,                  
+        null
+    )
+},
+{
+    (SpeciesType.Rooster, SexType.Male),
+    new AnimalProfile(
+        SpeciesType.Rooster, 
+        SexType.Male, 
+        FoodType.Seeds, 
+        0.18m,               
+        2,                   
+        6 * 30,              
+        null,                
+        8 * 365,               
+        null,                
+        15 * 365,             
+        false,               
+        null,                
+        null,                
+        null,                 
+        null              
+    )
+}
     };
 
     public static AnimalProfile Get(SpeciesType species, SexType sex)
